4f4e46fc0404c165c3127a678132074a0d1b5309
Bruno Silva <brsilva.eng@gmail.com>
2020-06-19 18:57:32 +0100

    fix setup_keyboard.sh

--- keyboard/setup_keyboard.sh
+++ keyboard/setup_keyboard.sh
@@ -11,19 +11,25 @@ function error {
   case "$ERR" in
     1) echo "Código de layout do keyboard não informado" ;;
     2) echo "Este script deve ser executado como 'sudo'" ;;
+    3) echo "Erro desconhecido" ;;
   esac
   echo "ex.: sudo ./$SCRIPT_NAME br"
   exit "$ERR"
 }
 
-[ "$1" = "" ] && error "1"
+[ "$1" == "" ] && error 1
 
 readonly KEYBOARD="$1"
 readonly BACKUP="yes"
 readonly FILE="/etc/default/keyboard"
 
-if [ "$BACKUP" = "yes" ] ; then
+if [ "$BACKUP" == "yes" ] && [ ! -e "$FILE"_bkp ] ; then
   # cria um backup do /etc/default/keyboard
-  cp "$FILE" "$FILE"_bkp || error
+  cp "$FILE" "$FILE"_bkp &> /dev/null || error 2
 fi
-sed 's/XKBLAYOUT=".*"/XKBLAYOUT="$KEYBOARD"/g' "$FILE" > "$FILE" || error
+
+sed s/XKBLAYOUT=\".*\"/XKBLAYOUT=\""$KEYBOARD"\"/g "$FILE" 1> "temp" || error 3 
+cp "temp" "$FILE" &> /dev/null || error 2
+rm "temp"
+
+
